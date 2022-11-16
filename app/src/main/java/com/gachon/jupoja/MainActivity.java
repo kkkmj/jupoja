2ed7b392389aea0858e79caf0e26740a264ff9aa
kkkmj <wjsdus531@naver.com>
2022-11-17 02:54:28 +0900

    Initial commit

---
+++ app/src/main/java/com/gachon/jupoja/MainActivity.java
@@ -0,0 +1,38 @@
+package com.gachon.jupoja;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+import android.view.Window;
+import android.webkit.WebSettings;
+import android.webkit.WebView;
+import android.webkit.WebViewClient;
+
+public class MainActivity extends AppCompatActivity {
+    public WebView webView;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        requestWindowFeature(Window.FEATURE_NO_TITLE);
+        setContentView(R.layout.activity_main);
+
+        webView = (WebView)findViewById(R.id.webView);
+        WebSettings webSettings = webView.getSettings();
+        webSettings.setJavaScriptEnabled(true);
+
+        // 앱에서 표시할 url 입력
+        webView.loadUrl("http://43.200.179.55:8080/");
+        webView.setWebViewClient(new WebViewClient());
+    }
+
+    //폰의 뒤로가기 버튼의 동작 입력
+    @Override
+    public void onBackPressed() {
+        if(webView.canGoBack()) {
+            webView.goBack();
+        } else {
+            super.onBackPressed();
+        }
+    }
+}
